fb7cff35165955698766b913d25265d00c54cc91
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-03-24 16:40:48 +0000

    Added local run mode to connect to local DB

--- src/dbClient.js
+++ src/dbClient.js
@@ -3,16 +3,23 @@ const logger = require('../logger').initLogger()
 
 let client
 
+let dbURI = process.env.MONGO_URI
+let dbName = 'covid19'
+
+if (process.env.LOCAL) {
+  dbURI = 'mongodb://localhost:27017'
+}
+
 const connectDB = async () => {
   try {
-    client = await MongoClient.connect(process.env.MONGO_URI, { useUnifiedTopology: true })
+    client = await MongoClient.connect(dbURI, { useUnifiedTopology: true })
   } catch (error) {
     logger.error(error)
   }
 }
 
 const disconnectDB = () => client.close()
-const getDBClient = () => client.db()
+const getDBClient = () => client.db(dbName)
 const getClient = () => client
 
 module.exports = {
